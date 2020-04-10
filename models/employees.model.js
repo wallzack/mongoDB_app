4bf44b283375e4229a0b9820fc0bdb6c168de224
wallzack <vulcheek@gmail.com>
2020-04-10 17:17:46 +0200

    23.3 added mongoose to departmen.routes.js

---
+++ models/employees.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+
+const employeesSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  department: { type: String, required: true }
+});
